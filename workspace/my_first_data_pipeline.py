f111d2b8a4886088764f803acf74b3ad0577a19c
zkan <zkan.cs@gmail.com>
2024-01-13 18:47:04 +0700

    Prepare code for simple data pipeline workshop

---
+++ workspace/my_first_data_pipeline.py
@@ -0,0 +1,33 @@
+import json
+
+import requests
+
+
+if __name__ == "__main__":
+    # Get data from Dog API
+    # โค้ดส่วนนี้จะเป็นการเชื่อมต่อกับ Dog API โดยเราจะใส่ URL Endpoint
+    url = "https://dog.ceo/api/breeds/image/random"
+    # เสร็จแล้วจะยิง Request ไปดึงข้อมูลมาเก็บใส่ Response
+    response = requests.get(url)
+    data = response.json()
+    print(data)
+
+    # Write data to file
+    with open("dogs.json", "w") as f:
+        json.dump(data, f)
+
+    # โค้ดส่วนนี้จะเป็นการเตรียมเรื่อง Credentials และ Configuration ต่าง ๆ เพื่อใช้ในการเชื่อมต่อกับ Dog API
+    api_url = "https://api.jsonbin.io/v3/b"
+    headers = {
+        "Content-Type": "application/json",
+        "X-Master-Key": "YOUR_X_MASTER_KEY",
+        "X-Collection-Id": "YOUR_COLLECTION_ID",
+    }
+
+    # Read data from file
+    with open("dogs.json", "r") as f:
+        data = json.load(f)
+
+    # โค้ดส่วนนี้จะเป็นการยิง Request ไปเพื่ออัพโหลดข้อมูลไปยังเซิฟเวอร์ของ JSONBin
+    response = requests.post(api_url, json=data, headers=headers)
+    print(response.json())
